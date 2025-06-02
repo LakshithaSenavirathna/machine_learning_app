dbd8dae79c3724f08222e17d6d275115d054c82b
LakshithaSenavirathna <srimalsenavirathna44@gmail.com>
2025-06-02 18:49:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,5 +90,5 @@ prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
 df_prediction_proba.rename(columns={0: 'Adelie',
                                     1: 'Chinstrap',
-                                    2: 'Gentoo'}
+                                    2: 'Gentoo'})
 df_prediction_proba                          
